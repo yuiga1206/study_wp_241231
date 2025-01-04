88eb7af3300b4b1d3a3b6a25945431be9b98b57d
yuta adachi <info@rcwas.com>
2025-01-04 15:31:20 +0900

    カスタム投稿「food」：タクソノミーごとに表示を切り分ける。

--- wp-content/themes/food-science/archive-food.php
+++ wp-content/themes/food-science/archive-food.php
@@ -7,18 +7,49 @@
           <h2 class="heading heading-primary"><span>フード紹介</span>FOOD</h2>
         </div>
 
-        <section class="section_body">
-          <h3 class="heading heading-secondary">お食事<span>MEAL</span></h3>
-          <ul class="foodList">
-            <?php if(have_posts()): ?>
-              <?php while(have_posts()): the_post(); ?>
-                <li class="foodList_item">
-                  <?php get_template_part('template-parts/loop', 'food'); ?>
-                </li>
-              <?php endwhile; ?>
-            <?php endif; ?>
-          </ul>
-        </section>
+        <?php
+          $menu_terms_args = [
+            'taxonomy' => 'menu',
+          ];
+          $menu_terms = get_terms($menu_terms_args);
+          if(!empty($menu_terms)):
+        ?>
+        <?php foreach($menu_terms as $menu): ?>
+          <section class="section_body">
+            <h3 class="heading heading-secondary"><a href="<?php echo get_term_link($menu); ?>"><?php echo $menu->name; ?></a><span><?php echo strtoupper($menu->slug); ?></span></h3>
+            <ul class="foodList">
+              <?php
+                // メニューの投稿タイプ
+                $args = [
+                  'post_type' => 'food',
+                  'posts_per_page' => -1,
+                ];
+                // メニューの種類で絞り込む
+                $taxquerysp = [
+                  'relation' => 'AND',
+                ];
+                $taxquerysp[] = [
+                  'taxonomy' => 'menu',
+                  'terms' => $menu->slug,
+                  'field' => 'slug',
+                ];
+                $args['tax_query'] = $taxquerysp;
+                $the_query = new WP_Query($args);
+                if($the_query->have_posts()):
+              ?>
+                <?php while($the_query->have_posts()): $the_query->the_post(); ?>
+                  <li class="foodList_item">
+                    <?php get_template_part('template-parts/loop', 'food'); ?>
+                  </li>
+                <?php 
+                  endwhile;
+                  wp_reset_postdata();
+                ?>
+              <?php endif; ?>
+            </ul>
+          </section>
+        <?php endforeach; ?>
+        <?php endif; ?>
 
       </div>
     </section>
---
+++ wp-content/themes/food-science/taxonomy.php
@@ -0,0 +1,31 @@
+<?php
+  // 開いているタクソノミーページの情報を取得
+  $menu_slug = get_query_var('menu');
+  $menu = get_term_by('slug', $menu_slug, 'menu');
+?>
+<?php get_header(); ?>
+
+  <main>
+    <section class="section section-foodList">
+      <div class="section_inner">
+        <div class="section_header">
+          <h2 class="heading heading-primary"><span>フード紹介</span>FOOD</h2>
+        </div>
+
+        <section class="section_body">
+          <h3 class="heading heading-secondary"><?php single_term_title(); ?><span><?php echo strtoupper($menu->slug); ?></span></h3>
+          <ul class="foodList">
+            <?php if(have_posts()): ?>
+              <?php while(have_posts()): the_post(); ?>
+                <li class="foodList_item">
+                  <?php get_template_part('template-parts/loop', 'food'); ?>
+                </li>
+              <?php endwhile; ?>
+            <?php endif; ?>
+          </ul>
+        </section>
+      </div>
+    </section>
+  </main>
+
+<?php get_footer(); ?>
